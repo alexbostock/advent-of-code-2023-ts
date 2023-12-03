d8ab8647873e51cb2b4e2ce73ef4cb3f0e6241c6
Alex Bostock <alex@bostock.uk>
2023-12-03 20:49:15 +0000

    feat: implement day 2: cube conundrum (part 1)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { open } from 'node:fs/promises';
 import { getCalibrationValue } from './lib/1-trebuchet.js';
+import { gameId, isPossible } from './lib/2-cube-conundrum.js';
 
 async function trebuchet() {
   const inputFile = await open('input/1-trebuchet.txt', 'r');
@@ -13,4 +14,31 @@ async function trebuchet() {
   console.log(sum);
 }
 
-await trebuchet();
+async function cubeConundrum() {
+  const inputFile = await open('input/2-cube-conundrum.txt');
+
+  let sum = 0;
+
+  for await (const line of inputFile.readLines()) {
+    if (isPossible(line, { red: 12, green: 13, blue: 14 })) {
+      sum += gameId(line);
+    }
+  }
+
+  console.log(sum);
+}
+
+const puzzleKey = process.argv[2];
+
+const puzzleMap: Record<string, () => Promise<void>> = {
+  '1': trebuchet,
+  '2': cubeConundrum,
+};
+
+const puzzle = puzzleMap[puzzleKey];
+
+if (!puzzle) {
+  throw new Error(`Cannot find puzzle ${puzzleKey}`);
+}
+
+await puzzle();
